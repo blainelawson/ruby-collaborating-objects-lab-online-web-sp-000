21e6a17f9e347729bc536e9491ebe71978ca0a46
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-15 17:48:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,17 +2,15 @@ class Song
   attr_accessor :name, :artist, :genre
   @@all = []
 
-  def initialize(song, artist="",genre="")
+  def initialize(song)
     # binding.pry
     @name = song
-    @genre = genre
-    @artist = artist
     @@all << self
   end
 
   def self.new_by_filename(file_name)
     # binding.pry
-    @name = self.new(file_name.split(" - ")[1], file_name.split(" - ")[0], file_name.split(" - ")[2].gsub(".mp3",""))
+    @name = self.new(file_name.split(" - ")[1])
     Artist.find_or_create_by_name(@name)
     binding.pry
     @name

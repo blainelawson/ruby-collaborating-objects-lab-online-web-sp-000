30fa3b358ccffeee90401f46aa5759279a1373d8
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-15 14:06:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,7 +26,7 @@ class Artist
 
   def self.find_or_create_by_name(name)
     if !self.all.find do |i|
-      # binding.pry
+      binding.pry
       i.artist.name == name
     end
       @name = name

22433bc51a7041e3641dfa7f5c02fac77d756cb9
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-15 18:01:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
     song = self.new(file_name.split(" - ")[1])
     artist = Artist.find_or_create_by_name(file_name.split(" - ")[0])
     artist.add_song(song)
-    
+
     song
   end
 end

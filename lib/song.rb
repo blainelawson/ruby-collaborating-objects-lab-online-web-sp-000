ba32dca27a12ce79cb62c79b872f78c8b2c67814
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-15 17:54:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,5 +11,8 @@ class Song
   def self.new_by_filename(file_name)
     # binding.pry
     song = self.new(file_name.split(" - ")[1])
+    artist = Artist.find_or_create_by_name(file_name.split(" - ")[0])
+    binding.pry
+    @name
   end
 end

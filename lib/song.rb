3e2b464addaced8fed3faae1f313588f8fd44ea3
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-15 14:29:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
   def self.new_by_filename(file_name)
     binding.pry
     @name = self.new(file_name.split(" - ")[1], file_name.split(" - ")[0], file_name.split(" - ")[2].gsub(".mp3",""))
-
+    @@all << self
 
   end
 end
